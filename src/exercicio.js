b20c65f4021b31d244cee4bd515ed7838753c7e2
Sidinei de Souza Lara <sidineilara@gmail.com>
2024-09-12 21:40:21 -0300

    commit inicial

---
+++ src/exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const somaNumeros = (a, b) => a * b;
+const saudacao = (nome) => `Olá ${nome}`;
+console.log(somaNumeros(23, 2));
+console.log(saudacao("Sidinei"));
---
+++ src/exercicio.ts
@@ -0,0 +1,6 @@
+const somaNumeros = (a: number, b: number): number => a * b;
+
+const saudacao = (nome: string): string => `Olá ${nome}`;
+
+console.log(somaNumeros(23, 2));
+console.log(saudacao("Sidinei"));
